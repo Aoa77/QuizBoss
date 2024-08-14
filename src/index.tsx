637c602515cda855ba24476e3d3551d2d68b78eb
aoa77 <alalbers77@gmail.com>
2024-08-14 12:18:52 -0500

    working on modules for quiz data

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import { QuizApp } from "./components/QuizApp";
+// import { QuizApp } from "./components/QuizApp";
 import ReactDOM from "react-dom/client";
-import jsonData from "./data/WorldFlags.json";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-    <QuizApp
-        quizTitle="World Flags Quiz"
-        itemQuestion="Which country does this flag belong to?"
-        jsonData={jsonData}
-        // imageLoadThrottle={50}
-        // resultDisplayTime={1500}
-        // spinnerPollingDelay={500}
-        // spinnerPollingInterval={100}
-    />,
+    <h1>ok</h1>
+    // <QuizApp
+    //     quizTitle="World Flags Quiz"
+    //     itemQuestion="Which country does this flag belong to?"
+    //     jsonData={jsonData}
+    //     // imageLoadThrottle={50}
+    //     // resultDisplayTime={1500}
+    //     // spinnerPollingDelay={500}
+    //     // spinnerPollingInterval={100}
+    // />,
 );
