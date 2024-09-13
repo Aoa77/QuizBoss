4fcce30e20a971420941225d880c42910b57d764
aoa77 <alalbers77@gmail.com>
2024-09-13 13:32:29 -0500

    version scripts

--- scripts/update-app-version.js
+++ scripts/update-app-version.js
@@ -2,13 +2,13 @@ import fs from "fs";
 import readline from "readline";
 
 console.log("<update-app-version>\n***");
-updateAppVersion();
+await updateAppVersion();
 console.log("***\n</update-app-version>\n\n");
 
 export default async function updateAppVersion() {
     const FILE_PATH = "src/components/AppVersion.tsx";
     return new Promise((resolve, reject) => {
-        console.log(`Updating ${FILE_PATH}...`);
+        console.log(`Updating file: "${FILE_PATH}"`);
 
         const reader = readline.createInterface({
             input: fs.createReadStream(FILE_PATH),
@@ -19,7 +19,6 @@ export default async function updateAppVersion() {
         const updatedLines = [];
 
         reader.on("line", (line) => {
-            console.log(line);
             if (line.indexOf("const VERSION") === 0) {
                 updatedLines.push(
                     `const VERSION = "${new Date().toISOString()}";`,
@@ -30,11 +29,7 @@ export default async function updateAppVersion() {
         });
 
         reader.on("close", () => {
-            console.log("");
-            console.log(updatedLines.join(""));
-            console.log("");
             fs.writeFileSync(FILE_PATH, updatedLines.join(""));
-            console.log(`Updated ${FILE_PATH}`);
             resolve();
         });
     });
--- src/components/AppVersion.tsx
+++ src/components/AppVersion.tsx
@@ -1,6 +1,7 @@
 import { AppContext } from "../app";
-const VERSION = "2024-09-13T18:07:25.875Z";
-
+///////////////////////////////////////////
+const VERSION = "2024-09-13T18:27:51.861Z";
+///////////////////////////////////////////
 export default function AppVersion(context: AppContext) {
     const { elements: elementController } = context;
     return <h6 ref={elementController.refs.appVersion}>{VERSION}</h6>;
