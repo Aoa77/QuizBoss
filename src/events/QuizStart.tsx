407bd3ef69bd27ae84b5a2c2950fa57be70df01f
aoa77 <alalbers77@gmail.com>
2024-10-23 13:48:35 -0500

    anim trans

--- src/events/QuizStart.tsx
+++ src/events/QuizStart.tsx
@@ -2,18 +2,24 @@ import { LoadingProgress } from "../components/LoadingProgress";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import { QuizTitle } from "../components/QuizTitle";
 import { FlowContext } from "../libs/flow-context/FlowContext";
-import { TaskGroup } from "../libs/friendlies/Task";
+import { Task, TaskGroup } from "../libs/friendlies/Task";
 import { EventName } from "../models/EventName";
 import { QuizState } from "../models/QuizState";
 
 export async function QuizStart() {
     //
     const anims = TaskGroup.create();
-    anims.add(LoadingProgress.animation.transitionOut());
-    anims.add(LoadingSpinner.animation.transitionIn());
-    anims.add(QuizTitle.animation.transitionIn());
+    anims.add(
+        LoadingProgress.animation
+            .transitionOut()
+            .then(() => LoadingSpinner.animation.transitionIn()),
+    );
+    anims.add(
+        Task.delay(LoadingProgress.config.animationDuration! / 2) /////////
+            .then(() => QuizTitle.animation.transitionIn()),
+    );
     await anims.all();
-    
+
     //
     const [state, setState] = FlowContext.current<QuizState>();
     setState({ ...state, eventName: EventName.NextQuestion });
