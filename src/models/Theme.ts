86321a518436df2c59132d145a490edaf11400ae
aoa77 <alalbers77@gmail.com>
2024-10-26 10:37:32 -0500

    css var names

--- src/models/Theme.ts
+++ src/models/Theme.ts
@@ -11,56 +11,56 @@ export enum ThemeName {
 
 export enum TV {
     ///
-    BODY_backgroundColor                 = "--tv_BODY_backgroundColor",
-    MAIN_backgroundColor                 = "--tv_MAIN_backgroundColor",
+    BODY_backgroundColor                 = "--tv-BODY_backgroundColor",
+    MAIN_backgroundColor                 = "--tv-MAIN_backgroundColor",
 
     ///
-    GuessButton_CORRECT_backgroundColor  = "--tv_GuessButton_CORRECT_backgroundColor",
-    GuessButton_CORRECT_borderColor      = "--tv_GuessButton_CORRECT_borderColor",
-    GuessButton_CORRECT_color            = "--tv_GuessButton_CORRECT_color",
+    GuessButton_CORRECT_backgroundColor  = "--tv-GuessButton_CORRECT_backgroundColor",
+    GuessButton_CORRECT_borderColor      = "--tv-GuessButton_CORRECT_borderColor",
+    GuessButton_CORRECT_color            = "--tv-GuessButton_CORRECT_color",
 
     ///
-    GuessButton_DIMMED_backgroundColor   = "--tv_GuessButton_DIMMED_backgroundColor",
-    GuessButton_DIMMED_borderColor       = "--tv_GuessButton_DIMMED_borderColor",
-    GuessButton_DIMMED_color             = "--tv_GuessButton_DIMMED_color",
+    GuessButton_DIMMED_backgroundColor   = "--tv-GuessButton_DIMMED_backgroundColor",
+    GuessButton_DIMMED_borderColor       = "--tv-GuessButton_DIMMED_borderColor",
+    GuessButton_DIMMED_color             = "--tv-GuessButton_DIMMED_color",
 
     ///
-    GuessButton_NORMAL_backgroundColor   = "--tv_GuessButton_NORMAL_backgroundColor",
-    GuessButton_NORMAL_borderColor       = "--tv_GuessButton_NORMAL_borderColor",
-    GuessButton_NORMAL_color             = "--tv_GuessButton_NORMAL_color",
+    GuessButton_NORMAL_backgroundColor   = "--tv-GuessButton_NORMAL_backgroundColor",
+    GuessButton_NORMAL_borderColor       = "--tv-GuessButton_NORMAL_borderColor",
+    GuessButton_NORMAL_color             = "--tv-GuessButton_NORMAL_color",
 
     ///
-    GuessButton_REVEAL_backgroundColor   = "--tv_GuessButton_REVEAL_backgroundColor",
-    GuessButton_REVEAL_borderColor       = "--tv_GuessButton_REVEAL_borderColor",
-    GuessButton_REVEAL_color             = "--tv_GuessButton_REVEAL_color",
+    GuessButton_REVEAL_backgroundColor   = "--tv-GuessButton_REVEAL_backgroundColor",
+    GuessButton_REVEAL_borderColor       = "--tv-GuessButton_REVEAL_borderColor",
+    GuessButton_REVEAL_color             = "--tv-GuessButton_REVEAL_color",
 
     ///
-    GuessButton_WRONG_backgroundColor    = "--tv_GuessButton_WRONG_backgroundColor",
-    GuessButton_WRONG_borderColor        = "--tv_GuessButton_WRONG_borderColor",
-    GuessButton_WRONG_color              = "--tv_GuessButton_WRONG_color",
+    GuessButton_WRONG_backgroundColor    = "--tv-GuessButton_WRONG_backgroundColor",
+    GuessButton_WRONG_borderColor        = "--tv-GuessButton_WRONG_borderColor",
+    GuessButton_WRONG_color              = "--tv-GuessButton_WRONG_color",
     
     ///
-    LoadingSpinner_fill                  = "--tv_LoadingSpinner_fill",
-    LoadingProgress_BAR_backgroundColor  = "--tv_LoadingProgress_BAR_backgroundColor",
-    LoadingProgress_BAR_color            = "--tv_LoadingProgress_BAR_color",
-    LoadingProgress_BAR_width            = "--tv_LoadingProgress_BAR_width",
-    LoadingProgress_TEXT_color           = "--tv_LoadingProgress_TEXT_color",
+    LoadingSpinner_fill                  = "--tv-LoadingSpinner_fill",
+    LoadingProgress_BAR_backgroundColor  = "--tv-LoadingProgress_BAR_backgroundColor",
+    LoadingProgress_BAR_color            = "--tv-LoadingProgress_BAR_color",
+    LoadingProgress_BAR_width            = "--tv-LoadingProgress_BAR_width",
+    LoadingProgress_TEXT_color           = "--tv-LoadingProgress_TEXT_color",
     
     ///
-    QuizTitle_color                      = "--tv_QuizTitle_color",
+    QuizTitle_color                      = "--tv-QuizTitle_color",
 
 
 
-    actionLink = "--tv_actionLink",
-    bonus = "--tv_bonus",
+    actionLink = "--tv-actionLink",
+    bonus = "--tv-bonus",
     
-    iconBackground = "--tv_iconBackground",
-    iconForeground = "--tv_iconForeground",
-    imageShadow = "--tv_imageShadow",
-    menuBackground = "--tv_menuBackground",
-    noBonus = "--tv_noBonus",
-    progress = "--tv_progress",
-    questionHeading = "--tv_questionHeading",
-    scoreLabel = "--tv_scoreLabel",
-    scoreValue = "--tv_scoreValue",
+    iconBackground = "--tv-iconBackground",
+    iconForeground = "--tv-iconForeground",
+    imageShadow = "--tv-imageShadow",
+    menuBackground = "--tv-menuBackground",
+    noBonus = "--tv-noBonus",
+    progress = "--tv-progress",
+    questionHeading = "--tv-questionHeading",
+    scoreLabel = "--tv-scoreLabel",
+    scoreValue = "--tv-scoreValue",
 }
