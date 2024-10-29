029e58c2233498e099e1ef2e5cd8483f67492fba
aoa77 <alalbers77@gmail.com>
2024-10-29 00:43:29 -0500

    reset

--- src/events/ConcludeCorrectGuess.ts
+++ src/events/ConcludeCorrectGuess.ts
@@ -1,4 +1,4 @@
-import { Duration, Scale } from "../libs/anime-context/AnimeContext.constants";
+import { Duration, Fade, Scale } from "../libs/anime-context/AnimeContext.constants";
 import { FlowContext } from "../libs/flow-context/FlowContext";
 import { TaskGroup } from "../libs/friendlies/Task";
 import { Anime } from "../models/Anime";
@@ -16,23 +16,36 @@ export async function ConcludeCorrectGuess() {
         if (bidx === state.guessButtonIndex) {
             return;
         }
+        const button = Anime.GuessButton(bidx);
         anims.add(
-            Anime.GuessButton(bidx).run({
-                scale: Scale.zero,
-                delay: 0.25 * duration,
-                duration,
-                easing: "easeOutElastic(3, 1)",
-            }),
+            button
+                .run({
+                    scale: Scale.zero,
+                    delay: 0.25 * duration,
+                    duration,
+                    easing: "easeOutElastic(3, 1)",
+                })
+                .then(() => {
+                    button.opacity = Fade.out;
+                    button.scale = Scale.one;
+                }),
         );
     });
 
     await anims.all();
-    await Anime.GuessButton(state.guessButtonIndex).run({
-        scale: Scale.zero,
-        delay: 0.25 * duration,
-        duration,
-        easing: "easeOutElastic(3, 0.75)",
-    });
+    
+    const button = Anime.GuessButton(state.guessButtonIndex);
+    await button
+        .run({
+            scale: Scale.zero,
+            delay: 0.25 * duration,
+            duration,
+            easing: "easeOutElastic(3, 0.75)",
+        })
+        .then(() => {
+            button.opacity = Fade.out;
+            button.scale = Scale.one;
+        });
 
     ///
     setState({ ...state, eventName: EventName.AwaitGuess });
