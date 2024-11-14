710636575e85bdc0fb130f0d009d0a2b9dd50ead
aoa77 <alalbers77@gmail.com>
2024-11-13 18:38:00 -0600

    [dev] deployment

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,11 +14,18 @@ set -x  # Enable debugging mode
 # save the git branch name in a string variable
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# write the current date and time to the version file
-echo $(date +"%Y-%m-%d %H:%M:%S") >./public/version
-
-# commit the changes
+# commit all changes
 git add . && git commit -m "[$branch] deployment"
+
+# save the commit hash in a string variable
+commit=$(git rev-parse --short HEAD)
+
+# save the current date and time in a string variable
+vdate = $(date +"%Y-%m-%d %H:%M:%S");
+
+# update the version file
+echo "[$branch] $commit" >./public/version
+echo $vdate >>./public/version
 
 # write the current branch and commit hash to the version file
 # echo $(git rev-parse --abbrev-ref HEAD) >>./public/version
