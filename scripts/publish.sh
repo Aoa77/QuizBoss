269d9b4e006cceeaf769918996fab163f779c651
aoa77 <alalbers77@gmail.com>
2024-11-13 18:41:39 -0600

    version [dev:1edf520] 2024-11-13 19:27:16

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,27 +8,45 @@ line=$(printf "%${LEN}s" | tr " " "$CHAR")
 printf "\n%s\n%s\n%s\n\n" "$line" "$script" "$line"
 printf "Current directory:\n$(pwd)\n\n"
 ###############################################################
-set -e  # Exit immediately on error
-set -x  # Enable debugging mode
+set -e # Exit immediately on error
+# set -x # Enable debugging mode
 ###############################################################
+# save the current date and time in a string variable
+vdate=$(date +"%Y-%m-%d %H:%M:%S")
+
 # save the git branch name in a string variable
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# commit all changes
-git add . && git commit -m "[$branch] deployment"
+# check if there are any changes to commit
+if [ -n "$(git status --porcelain)" ]; then
+    # commit all changes
+    git add . && git commit -m "publishing [$branch]"
+else
+    ##########################################
+    echo "No changes. Using last commit..."
+    ##########################################
+fi
 
 # save the commit hash in a string variable
 commit=$(git rev-parse --short HEAD)
 
 # save the current date and time in a string variable
-vdate=$(date +"%Y-%m-%d %H:%M:%S");
+vdate=$(date +"%Y-%m-%d %H:%M:%S")
+
+# format the string to be used as a version
+vtext="version [$branch:$commit] $vdate"
 
 # update the version file
-echo "[$branch] $commit $vdate" >./public/version
+echo $vtext >./public/version
 
-# write the current branch and commit hash to the version file
-# echo $(git rev-parse --abbrev-ref HEAD) >>./public/version
+# commit the version file change
+git add . && git commit -m "$vtext"
 
-#####
-# tsc -b && vite build
+# combine the last two commits into one, keeping the commit hash from the first one
+git reset --soft HEAD~2
+git commit --amend -m "$vtext"
+
+# build and push
+tsc -b && vite build
+git push --force
 ###############################################################
