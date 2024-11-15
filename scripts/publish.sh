1e2e0ddc33303b2f550869425e00f148bac137d9
aoa77 <alalbers77@gmail.com>
2024-11-14 19:26:04 -0600

    publishing [dev]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,7 @@ printf "\n%s\n%s\n%s\n\n" "$line" "$script" "$line"
 printf "Current directory:\n$(pwd)\n\n"
 ###############################################################
 set -e # Exit immediately on error
-# set -x # Enable debugging mode
+set -x # Enable debugging mode
 ###############################################################
 # save the current date and time in a string variable
 vdate=$(date +"%Y-%m-%d %H:%M:%S")
